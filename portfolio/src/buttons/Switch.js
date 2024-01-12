32218244844ca219a51ea075cba719eb192f1823
mgj0226 <michael27mic@gmail.com>
2024-01-12 17:34:08 +0000

    improve with useRef

--- portfolio/src/buttons/Switch.js
+++ portfolio/src/buttons/Switch.js
@@ -1,44 +1,44 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import "./Switch.css";
 import { useTheme } from "../ThemeContext";
 
 const Switch = () => {
- const { theme, toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
+  const switchRef = useRef(null);
+  const lightBaseRef = useRef(null);
+  const sunRef = useRef(null);
+  const moonRef = useRef(null);
 
  useEffect(() => {
-  const switchElement = document.querySelector('.switch');
-  const lightBaseElement = document.querySelector('#darkModeSwitch #lightBase');
-  const sunElement = document.querySelector('#darkModeSwitch #sun');
-  const moonElement = document.querySelector('#darkModeSwitch #moon');
-
   const handleClick = () => {
     if (theme === 'light') {
-      lightBaseElement.classList.remove('fadeIn');
-      lightBaseElement.classList.add('fadeOut');
-      sunElement.classList.remove('sunIn');
-      sunElement.classList.add('sunOut');
-      moonElement.classList.remove('moonOut');
-      moonElement.classList.add('moonIn');
+      lightBaseRef.current.classList.remove ('fadeIn');
+      lightBaseRef.current.classList.add ('fadeOut');
+      sunRef.current.classList.remove ('sunIn');
+      sunRef.current.classList.add ('sunOut');
+      moonRef.current.classList.remove ('moonOut');
+      moonRef.current.classList.add ('moonIn');
+
     } else if (theme === 'dark') {
-      lightBaseElement.classList.remove('fadeOut');
-      lightBaseElement.classList.add('fadeIn');
-      sunElement.classList.remove('sunOut');
-      sunElement.classList.add('sunIn');
-      moonElement.classList.remove('moonIn');
-      moonElement.classList.add('moonOut');
+      lightBaseRef.current.classList.remove ('fadeOut');
+      lightBaseRef.current.classList.add ('fadeIn');
+      sunRef.current.classList.remove ('sunOut');
+      sunRef.current.classList.add ('sunIn');
+      moonRef.current.classList.remove ('moonIn');
+      moonRef.current.classList.add ('moonOut');
     }
   };
 
-  switchElement.addEventListener('click', handleClick);
+  switchRef.current.addEventListener('click', handleClick);
 
   // Clean up function to remove the event listener when the component unmounts
   return () => {
-    switchElement.removeEventListener('click', handleClick);
+    switchRef.current.removeEventListener('click', handleClick);
   };
 }, [theme]);
 
  return (
-   <label className="switch">
+   <label className="switch" ref={switchRef}>
      <input
        type="checkbox"
        id="toggle"
@@ -48,12 +48,12 @@ const Switch = () => {
     <svg width="120" height="100" viewBox="0 0 120 100" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g id="darkModeSwitch">
 <rect id="darkBase" x="19" y="36" width="70" height="30" rx="15" fill="#636363"/>
-<g id="moon">
+<g id="moon" ref={moonRef}>
 <circle id="Ellipse 1" cx="74" cy="51" r="13" fill="#D9D9D9"/>
 <circle id="Ellipse 2" cx="69.6667" cy="46.6667" r="8.66667" fill="#636363"/>
 </g>
-<rect id="lightBase" x="19" y="36" width="70" height="30" rx="15" fill="#87CEEB"/>
-<g id="sun">
+<rect id="lightBase" ref={lightBaseRef} x="19" y="36" width="70" height="30" rx="15" fill="#87CEEB"/>
+<g id="sun" ref={sunRef}>
 <circle id="Ellipse 3" cx="34" cy="51" r="13" fill="#FFE484" fill-opacity="0.7"/>
 <circle id="Ellipse 4" cx="34" cy="51" r="8.18518" fill="#FDB813"/>
 <g id="sunRay">
