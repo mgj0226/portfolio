653ab45241029f27585667c601ac02895ec9751a
mgj0226 <michael27mic@gmail.com>
2024-02-01 21:25:10 +0000

    glowyMenuBtn

--- portfolio/src/components/buttons/MenuBtn.js
+++ portfolio/src/components/buttons/MenuBtn.js
@@ -1,4 +1,18 @@
+import './buttons.css';
+
 const MenuBtn = ({menuBtnLightRef}) => {
+    const syncPointer = ({ x: pointerX, y: pointerY }) => {
+        const x = pointerX.toFixed(2);
+        const y = pointerY.toFixed(2);
+        const xp = (pointerX / window.innerWidth).toFixed(2);
+        const yp = (pointerY / window.innerHeight).toFixed(2);
+        document.documentElement.style.setProperty('--x', x);
+        document.documentElement.style.setProperty('--y', y);
+        document.documentElement.style.setProperty('--xp', xp);
+        document.documentElement.style.setProperty('--yp', yp);
+    };
+    document.body.addEventListener('pointermove', syncPointer);
+
     return (
         <>
             <svg width="42" height="42" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
