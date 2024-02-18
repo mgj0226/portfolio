e6aa1dd6a48cb0a3b11f9c7fee4ce23135e992c6
mgj0226 <michael27mic@gmail.com>
2024-02-18 12:38:36 +0000

    updating footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,3 +1,27 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import { Box, HStack, Link } from "@chakra-ui/react";
+
+const links = [
+    {
+        icon: faEnvelope,
+        url: 'mailto:michael27mic@gmail.com',
+        label: 'Email',
+    },
+    {
+      icon: faGithub,
+      url: 'https://github.com/mgj0226',
+      label: 'GitHub',
+    },
+    {
+        icon: faLinkedin,
+        url: "http://www.linkedin.com/in/chun-nam-chan-149b13292",
+        label: 'LinkedIn',
+    },
+];
+
 export default function Footer() {
     return (
         <footer className="Footer">
