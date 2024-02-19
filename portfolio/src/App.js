467a2d7bbbd6eedb9a3c82d57cd7285a2219dc8e
mgj0226 <michael27mic@gmail.com>
2024-02-19 13:08:40 +0000

    updated css added animations

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,15 +7,25 @@ import Footer from "./components/Footer";
 
 function App() {
   const [theme, setTheme] = useState('light');
+  const [themeBg, setThemeBg] = useState('lightBg');
+
+  useEffect(() => {
+    if (theme === 'light') {
+      setThemeBg('lightBg');
+    } else {
+      setThemeBg('darkBg');
+    }
+  }, [theme]);
+
 
   return (
-    <>
+    <div className={`App ${themeBg}`}>
       <Header
         theme={theme}
         setTheme={setTheme}/>
       <About/>
       <Footer/>
-    </>
+    </div>
   );
 }
 
