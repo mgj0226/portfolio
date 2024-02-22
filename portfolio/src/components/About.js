4b3cf1c016bf2988f2cae7de471cb6b92f71bd47
mgj0226 <michael27mic@gmail.com>
2024-02-22 10:33:09 +0000

    animations and logo setting

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,11 +1,25 @@
+import React, { useRef } from 'react';
 import logo from '../assets/wolfLogo2.svg';
+import photo from '../assets/photo.svg';
 
 const About = () => {
+  const logoRef = useRef(null);
+
+  const handleClick = () => {
+    logoRef.current.classList.remove('fadeIn');
+    logoRef.current.classList.add('fadeOut');
+    setTimeout(() => {
+      logoRef.current.classList.remove('fadeOut');
+      logoRef.current.classList.add('fadeIn');
+    }, 5000);
+  }
+
     return (
         <section className="About">
-          <img src={logo} className="logo" alt="logo" />
-          <article>
-            <h2>Michael Chan</h2>
+          <img src={logo} className="logo" alt="logo" ref={logoRef} onClick={handleClick}/>
+          <img src={photo} className="photo" alt="photo" />
+          <article className='AboutArticle'>
+            <h2 className='AboutTitle'>Michael Chan</h2>
           </article>
 
         </section>
