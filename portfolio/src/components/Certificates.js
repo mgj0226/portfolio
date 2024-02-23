67bed8d4c4d26180b5be873f580c1bce88f19162
mgj0226 <michael27mic@gmail.com>
2024-02-23 17:55:12 +0000

    added Little to Projects

--- portfolio/src/components/Certificates.js
+++ portfolio/src/components/Certificates.js
@@ -1,7 +1,7 @@
 const Certificates = () => {
     return (
         <section className="Certificates">
-        <h1>Certificates</h1>
+        <h1>Skills and Certificates</h1>
         </section>
     )
 }
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,7 +1,30 @@
+import LemonHome1 from '../assets/LemonHome1.png';
+import LemonHome2 from '../assets/LemonHome2.png';
+import LemonBooking1 from '../assets/LemonBooking1.png';
+
 const Projects = () => {
     return (
         <section className="Projects">
         <h1>Projects</h1>
+        <div className="Project">
+            <div className="Projectbox">
+                <h3>Little Lemon</h3>
+                <div className="ProjectImgs">
+                    <img src={LemonHome1} alt="Little Lemon Home Page" />
+                    <img src={LemonHome2} alt="Little Lemon Home Page" />
+                    <img src={LemonBooking1} alt="Little Lemon Booking Page" />
+                </div>
+            </div>
+            <div className="Projectbox">
+                <h3>Coming Soon</h3>
+            </div>
+            <div className="Projectbox">
+                <h3>Coming Soon</h3>
+            </div>
+            <div className="Projectbox">
+                <h3>More</h3>
+            </div>
+        </div>
         </section>
     )
 }
